342e1461cad0f5d01f055edaf8e02b3b781ceaa1
Angela Wang <wangkejing2007@gmail.com>
2020-06-16 14:49:44 +0700

    2020-06-16

--- New_Beginning/教孩子學編程/2-5 四色螺旋線條.py
+++ New_Beginning/教孩子學編程/2-5 四色螺旋線條.py
@@ -1,11 +1,11 @@
 import turtle
 t = turtle.Pen()
 t.width(2)
-turtle.bgcolor("black")
+turtle.bgcolor("black") #改變背景顏色
 colors = ["red","blue","green","orange"]
 t.speed(0)
-for x in range(100):
+for x in range(400):
     t.color(colors[x % 4])
-    t.forward(4 * x)
+    t.forward(2 * x)
     t.left(89)
 turtle.done()
