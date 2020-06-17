2ba68a067de997475d7dae88b9a1f5c343fa029a
Angela Wang <wangkejing2007@gmail.com>
2020-06-17 19:23:37 +0700

    confirm

--- New_Beginning/教孩子學編程/2-6 添加變量.py
+++ New_Beginning/教孩子學編程/2-6 添加變量.py
@@ -5,7 +5,7 @@ t.speed(0)
 turtle.bgcolor("black")
 colors = ["red","blue","yellow","green","orange","cyan"]
 sides = 3
-for x in range(400):
+for x in range(200):
     t.color(colors[x % sides])
-    t.forward(2 * x)
+    t.forward(4 * x)
     t.left(360 / sides + 1)
