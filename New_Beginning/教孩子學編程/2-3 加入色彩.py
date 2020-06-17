e4fee86b4912781f1bc2f7f59013ce0ac49b160a
Angela Wang <wangkejing2007@gmail.com>
2020-06-17 11:09:00 +0700

    confirm

--- New_Beginning/教孩子學編程/2-3 加入色彩.py
+++ New_Beginning/教孩子學編程/2-3 加入色彩.py
@@ -1,13 +1,9 @@
 import turtle
 t = turtle.Pen()
-t.width(3)
-t.color("#ff00ff")
+t.width(2)
+t.color("blue")
 t.speed(0)
 for x in range(100):
-    t.forward(4 * x)
-<<<<<<< HEAD:New_Beginning/教孩子學編程/2-3 加入色彩.py
-    t.left(91)
+    t.circle(x)
+    t.left(60)
 turtle.done()
-=======
-    t.left(122)
->>>>>>> 4319029f923f25c995d426cb242834a7f09958eb:New_Beginning/教孩子學編程/2020-06-15.py
