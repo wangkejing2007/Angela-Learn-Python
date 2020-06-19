85d972e0721bd2c04219f785a4783abed068fa27
Angela Wang <wangkejing2007@gmail.com>
2020-06-19 20:50:35 +0700

    2020-06-19

--- New_Beginning/教孩子學編程/雞兔同籠.py
+++ New_Beginning/教孩子學編程/雞兔同籠.py
@@ -2,12 +2,12 @@ from sympy import *
 print()
 
 #一元一次
-#已知雞加羊的數目為20隻，雞加羊共有50隻腳，求雞的數量
+#已知雞加兔的數目為20隻，雞加兔共有50隻腳，求雞的數量？
 x = Symbol('x') #雞的數目
 print(solve(2*x + 4*(20-x)-50,x))
 print()
 
-#二元一次 
+#二元一次比較 
 y,z = symbols('y z')
 print(solve([y+z-10, 2*y+z-16], [y,z]))
 print()
