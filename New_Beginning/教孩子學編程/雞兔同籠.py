71aab3fdea8b1a95afb233cd9cb475293b999321
Angela Wang <wangkejing2007@gmail.com>
2020-06-23 12:04:49 +0700

    2020-06-23

--- New_Beginning/教孩子學編程/雞兔同籠.py
+++ New_Beginning/教孩子學編程/雞兔同籠.py
@@ -3,11 +3,12 @@ print()
 
 #一元一次
 #已知雞加兔的數目為20隻，雞加兔共有50隻腳，求雞的數量？
-x = Symbol('x') #雞的數目
+#一元一次(symbol)
+x = Symbol('x') #x表示雞的數目
 print(solve(2*x + 4*(20-x)-50,x))
 print()
 
 #二元一次(symbols)比較 
-y,z = symbols('y z')
-print(solve([y+z-10, 2*y+z-16], [y,z]))
+y,z = symbols('y z') #y表示雞的數目，z表示兔的數目
+print(solve([y+z-20, 2*y+4*z-50], [y,z]))
 print()
