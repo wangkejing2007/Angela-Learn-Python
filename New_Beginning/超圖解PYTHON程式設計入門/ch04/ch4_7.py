2e79f2d2800ca295291ed1a1763dbf0685c392cd
Angela Wang <wangkejing2007@gmail.com>
2020-07-14 08:51:58 +0700

    2020-7-14

--- New_Beginning/超圖解PYTHON程式設計入門/ch04/ch4_7.py
+++
@@ -1,8 +0,0 @@
-# 參閱4-15頁
-
-import os
-import time
-
-mt_sec = os.path.getmtime('照片.jpg')  # 請自行修改檔名
-mt = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(mt_sec))
-print('檔案修改時間：', mt)
--- New_Beginning/超圖解PYTHON程式設計入門/ch14/webcam_test.py
+++
@@ -1,9 +0,0 @@
-from webcam import Face
-
-dataset_file = 'dataset\\staff.dat'
-
-def get_RFID(id):
-    print('使用者的RFID:', id)
-
-face = Face(dataset_file, get_RFID)
-face.start()
