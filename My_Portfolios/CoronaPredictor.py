50736269df681e71a1166c6cbe93a68567a6ddba
Angela Wang <wangkejing2007@gmail.com>
2020-08-27 11:36:35 +0700

    00

--- My_Portfolios/CoronaPredictor.py
+++ My_Portfolios/CoronaPredictor.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomidalFeatures
+
 
 #### Load Data ####
 data = pd.read_csv('D:\Angela-Learn-Python\My_Portfolios\VNcase.csv', sep = ',')
-data = [['id','cases']]
-#print('-'*30);print('HEAD');print('-'*30)
-#print(data.head())
+data = data[['id','cases']]
+print('-'*30);print('HEAD');print('-'*30)
+print(data.head())
 
 
 #### Prepare Data ####
@@ -15,3 +17,7 @@ x = np.array(data['id']).reshape(-1, 1)
 y = np.array(data['cases']).reshape(-1, 1)
 plt.plot(y, '-m')
 plt.show()
+
+polyFeat = PolynomidalFeatures(degree=2)
+x = polyFeat.fit_transform(x)
+print(x)
